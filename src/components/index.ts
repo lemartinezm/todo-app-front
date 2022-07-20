c001989ed359c7dc250804c7607cc16398c62092
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-07-20 18:41:41 -0500

    Components modules modified

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
-export * from './Inputs';
+export * from './Auth';
 export * from './Confetti';
+export * from './Drawer';
+export * from './Inputs';
+export * from './NavBar';
+export * from './Todo';
